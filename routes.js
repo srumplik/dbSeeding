7e8f3e0b4430402086e13beacfea80a31e7184b3
Steven Rumplik <s_rumplik@yahoo.com>
2020-11-18 17:22:28 -0500

    update of notes and routes

--- routes.js
+++ routes.js
@@ -32,19 +32,19 @@ router.post('/login',function ( req, res ){
 
 // ROUTES FOR USERS =======================================
 router.get('/user/all', function ( req, res) {  // display ALL users
-	mongoose.model('User').find({}, function(err,users){
-		res.render('./user/users', {
-			users: users
+	mongoose.model('User').find({}, function(err,users){  // function to return all documents in the 'Users' collection
+		res.render('./user/users', {  // renders the 'users.pug' file
+			users: users  // sends the JSON data returned from the DB to the 'users.pug' file for rendering
 		});
 	});
 });
 
 router.get('/user/one/:id', function ( req, res ) {  //  display ONE user via its _id
-	mongoose.model('User').findOne({'_id': req.params.id}, function(err,user){
-		if(user == null)
+	mongoose.model('User').findOne({'_id': req.params.id}, function(err,user){  // function to return one document in the 'Users' colleciton with the '_id' passed in through the URL request
+		if(user == null)  // if the supplied '_id' is not in the 'Users' collection, return an 'error'
 		{
-			res.send('No user in database with that ID');
-		}else
+			res.send('No user in database with that ID');  // sending the 'error'
+		}else  // render the 'user.pug' file while passing the JSON data through
 		{
 			res.render('./user/user', {
 				user: user.username,
@@ -56,11 +56,11 @@ router.get('/user/one/:id', function ( req, res ) {  //  display ONE user via it
 });
 
 router.get('/user/create', function ( req, res) {  // page with form for creating a new user
-	res.render('./user/createUser');
+	res.render('./user/createUser');  // render the 'createUser.pug' file
 });
 
 router.post('/user/create', function ( req, res ) {  // add new user to DB
-	mongoose.model('User').create({username: req.body.username, password: req.body.password}, function(err, result) {
+	mongoose.model('User').create({username: req.body.username, password: req.body.password}, function(err, result) {  // function to save a new user document to the 'Users' collection
 		if (err){
 			res.send(err);
 		} else {
@@ -70,7 +70,7 @@ router.post('/user/create', function ( req, res ) {  // add new user to DB
 });
 
 router.post('/user/update/:id', function ( req, res ) {  // update user in DB via its _id
-	mongoose.model('User').findOneAndUpdate({ '_id': req.params.id}, req.body, function(err, user){
+	mongoose.model('User').findOneAndUpdate({ '_id': req.params.id}, req.body, function(err, user){  // function to find a user document that matches the '_id' passed in and update the document values with the newly supplied values
 		if(err){
 			console.log(err);
 		}else
@@ -78,20 +78,33 @@ router.post('/user/update/:id', function ( req, res ) {  // update user in DB vi
 			console.log(user)
 		};
 	});
-	res.redirect('/user/all');
+	res.redirect('/user/all');  // once completed, redirect to show all users and including the newly updated user
 });
 
 router.get('/user/delete/:id', function ( req, res ) {  // delete user from DB via its _id
-	mongoose.model('User').deleteOne({ '_id': req.params.id}, function(err, result){
+	mongoose.model('User').deleteOne({ '_id': req.params.id}, function(err, result){ // functino to find user document based on '_id' and remove it from 'Users' collection
 		if(err){
 			res.status(500).send(err);
 		}else
 		{
-			res.redirect('/user/all');
+			res.redirect('/user/all');  // redirect to show all users
 		};
 	});
 });
 
+
+
+// ROUTES FOR CLIENTS =======================================
+router.get('/client/all', function( req, res ){
+	res.send('List of all clients to come');
+}),
+
+
+
+// ROUTES FOR ASSETS =======================================
+router.get('/asset/all', function( req, res ){
+	res.send('List of all assets to come');
+}),
 
 
 module.exports = router
